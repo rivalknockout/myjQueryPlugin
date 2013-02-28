f0f2ed8cdf756a93a6ab8006349bb2c45d5b2379
hideaki <rivalknockout@gmail.com>
2013-02-28 19:56:01 +0900

    ドキュメントレディー後に読み込むようにした

--- pageup-applelike.js
+++ pageup-applelike.js
@@ -1,17 +1,19 @@
 (function($){
-	var $pa = $('<img id="pageup-applelike">').appendTo('body');
-	var SPEED = 600;
-	var EASE = 'easeInOutQuart';
-	$pa.attr({
-		src: 'https://raw.github.com/rivalknockout/myjQueryPlugin/master/pageup-applelike.png',
-		width: 130, height: 130
-	});
-	$pa.css({
-		display: 'block', position: 'fixed', right: 0, bottom: 0, cursor: 'pointer', zIndex: 999999999999
-	});
-	$pa.click(function(){
-		$(window).animate({
-			scrollTop: 0
-		}, SPEED, EASE);
+	$(function(){
+		var $pa = $('<img>').appendTo('body');
+		var SPEED = 600;
+		var EASE = 'easeInOutQuart';
+		$pa.attr({
+			src: 'https://raw.github.com/rivalknockout/myjQueryPlugin/master/pageup-applelike.png',
+			width: 130, height: 130
+		});
+		$pa.css({
+			display: 'block', position: 'fixed', right: 0, bottom: 0, cursor: 'pointer', zIndex: 99999
+		});
+		$pa.click(function(){
+			$(window).animate({
+				scrollTop: 0
+			}, SPEED, EASE);
+		});
 	});
 })(jQuery);
