27102e6d0ddc2266ecce31a71dafa46e753cd70a
hideaki <rivalknockout@gmail.com>
2013-03-01 14:05:52 +0900

    不要な記述を削除

--- pageup-applelike.js
+++ pageup-applelike.js
@@ -1,29 +1,27 @@
-(function($){
-	$(function(){
-		var $pa = $('<img>').appendTo('body');
-		var SPEED = 600;
-		var EASE = 'easeInOutQuart';
-		var SHOW_Y = 200;
-		//ini
-		$pa.attr({
-			src: 'https://raw.github.com/rivalknockout/myjQueryPlugin/master/pageup-applelike.png',
-			width: 130, height: 130
-		});
-		$pa.css({
-			display: 'none', position: 'fixed', right: 0, bottom: 0, cursor: 'pointer', zIndex: 99999
-		});
-		//observe the body scroll to fadeIn and fadeOut the corner arrow img.
-		$(window).scroll(function(){
-			if(window.scrollY>SHOW_Y)
-				$pa.fadeIn(500);
-			else if(window.scrollY<SHOW_Y)
-				$pa.fadeOut(500);
-		});
-		//event
-		$pa.click(function(){
-			$('body, html').stop(true, true).animate({
-				scrollTop: 0
-			}, SPEED, EASE);
-		});
+jQuery(function($){
+	var $pa		= $('<img>').appendTo('body');
+	var SPEED	= 600;
+	var EASE	= 'easeInOutQuart';
+	var SHOW_Y	= 200;
+	//ini
+	$pa.attr({
+		src: 'https://raw.github.com/rivalknockout/myjQueryPlugin/master/pageup-applelike.png',
+		width: 130, height: 130
 	});
-})(jQuery);
+	$pa.css({
+		display: 'none', position: 'fixed', right: 0, bottom: 0, cursor: 'pointer', zIndex: 99999
+	});
+	//observe the body scroll to fadeIn and fadeOut the corner arrow img.
+	$(window).scroll(function(){
+		if(window.scrollY>SHOW_Y)
+			$pa.fadeIn(500);
+		else if(window.scrollY<SHOW_Y)
+			$pa.fadeOut(500);
+	});
+	//event
+	$pa.click(function(){
+		$('body, html').stop(true, true).animate({
+			scrollTop: 0
+		}, SPEED, EASE);
+	});
+});
